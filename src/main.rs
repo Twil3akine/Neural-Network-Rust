574722d7e86674929a25f1989ee08555df81fa58
twil3akine <twil3akine@gmail.com>
2025-06-14 22:40:32 +0900

    Neuron, Layer, Model構造体を作成
    0 ~ 15 のxor の学習結果↓
    
    Last loss = 0.0310
    
    Result:
    Confusion Matrix:
           0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15
      0   15    0    0    0    0    0    0    0    0    0    0    0    0    0    0    0
      1    0   12    0    2    0    0    0    0    0    0    0    0    0    0    0    0
      2    0    0   13    0    0    0    1    0    0    0    0    0    0    0    0    0
      3    0    3    0    9    0    2    0    0    0    0    0    0    0    0    0    0
      4    0    0    0    0   14    0    0    0    0    0    0    0    0    0    0    0
      5    0    0    0    0    0   13    0    1    0    0    0    0    0    0    0    0
      6    0    0    4    0    0    0   10    0    0    0    0    0    0    0    0    0
      7    0    1    0    1    0    3    0    9    0    0    0    0    0    0    0    0
      8    0    0    0    0    0    0    0    0   14    0    0    0    0    0    0    0
      9    0    0    0    0    0    0    0    0    0   12    0    2    0    0    0    0
     10    0    0    0    0    0    0    1    0    0    0   12    1    0    0    0    0
     11    0    0    0    0    0    2    0    0    1    1    0   10    0    0    0    0
     12    0    0    0    0    1    0    0    0    0    0    1    0    9    1    2    0
     13    0    0    0    0    0    0    0    0    0    0    0    0    4   10    0    0
     14    0    0    0    0    0    0    0    0    0    0    0    0    1    0   12    1
     15    0    0    0    0    0    0    0    0    0    0    0    0    0    0    2   12

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,66 @@
 mod utils;
 
 fn main() {
-    let mut neuron = utils::Neuron::new(2);
+    let limit: usize = 100;
+    let mut model = utils::Model::new(2, 16);
 
-    let training_data: Vec<(Vec<f64>, f64)> = 
-        vec![
-            (vec![0.0, 0.0], 0.0),
-            (vec![0.0, 1.0], 0.0),
-            (vec![1.0, 0.0], 0.0),
-            (vec![1.0, 1.0], 1.0),
-        ];
+    let mut training_data: Vec<(Vec<f64>, Vec<f64>)> = Vec::new();
+    for x in 0..15 {
+        for y in 0..15 {
+            let xor = x^y;
+            let mut target = vec![0.0; 16];
+            target[xor] = 1.0;
+            training_data.push((vec![x as f64, y as f64], target));
+        }
+    }
 
-    for epoch in 0..1_000_000_000 {
+    for epoch in 0..100*limit {
+        let mut total_loss = 0.0;
         for (inputs, target) in &training_data {
-            neuron.train(inputs, *target, 0.005);
+            total_loss += model.train(inputs, target, 0.005);
         }
 
-        if epoch % (100_000) == 0 {
-            println!("Epoch {epoch}");
+        if epoch % limit == 0 {
+            let process: usize = epoch / limit + 1;
+            println!(
+                "Epoch {:02}: loss = {:.4}",
+                process,
+                total_loss / training_data.len() as f64
+            );
         }
     }
 
-    println!("テスト");
-    for (inputs, _) in &training_data {
-        let out = neuron.forward(inputs);
-        println!("{:?} => {:.4}", inputs, out);
+    println!("\n");
+
+    println!("Result:");
+
+    let mut confusion_matrix: Vec<Vec<usize>> = vec![vec![0usize; 16]; 16];
+    for (input, _target) in &training_data {
+        let output = model.forward(input);
+        let (predicted, _confidence) = output
+            .iter()
+            .enumerate()
+            .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+            .map(|(i, &v)| (i, v))
+            .unwrap();
+
+        let correct = (input[0] as usize) ^ (input[1] as usize);
+        confusion_matrix[correct][predicted] += 1;
     }
 
-    println!("Last weights: {:?}", neuron.weights);
-    println!("Last bias: {:?}", neuron.bias);
+    println!("Confusion Matrix:");
+    print!("     ");
+
+    for i in 0..16 {
+        print!("{:^5}", i);
+    }
+    println!();
+
+    for (i, row) in confusion_matrix.iter().enumerate() {
+        print!("{:^5}", i);
+        for val in row {
+            print!("{:^5}", val);
+        }
+        println!();
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,13 @@
 #![allow(dead_code)]
 #![allow(deprecated)]
 
-use rand::Rng;
+
+
+use core::f64;
+
+use rand::{Rng};
+
+
 
 pub fn linear(x: f64) -> f64 {
     x
@@ -11,21 +17,45 @@ pub fn relu(x: f64) -> f64 {
     if x < 0.0 { 0.0 } else { x }
 }
 
-pub fn step(x: f64) -> f64 {
-    if x < 0.0 { 0.0 } else { 1.0 }
-}
-
 pub fn sigmoid(x: f64) -> f64 {
     (1.0) / (1.0 + (-x).exp())
 }
 
+pub fn softmax(xs: &[f64]) -> Vec<f64> {
+    let max = xs
+        .iter()
+        .cloned()
+        .fold(f64::NEG_INFINITY, f64::max);
+
+    let exps: Vec<f64> = xs
+        .iter()
+        .map(|x| (x-max).exp())
+        .collect::<Vec<f64>>();
+
+    let sum: f64 = exps
+        .iter()
+        .sum::<f64>();
+
+    exps.iter().map(|x| x/sum).collect::<Vec<f64>>()
+}
+
 /*
- * sigmoid の微分
+ * 微分
  */
+pub fn linear_deriv(_x: f64) -> f64 {
+    1.0
+}
+
+pub fn relu_deriv(x: f64) -> f64 {
+    if x < 0.0 { 0.0 } else { 1.0 }
+}
+
 pub fn sigmoid_deriv(x: f64) -> f64 {
     let s: f64 = sigmoid(x);
     s * (1.0 - s)
 }
+
+
 
 pub struct Neuron {
     pub weights: Vec<f64>,
@@ -33,10 +63,10 @@ pub struct Neuron {
 }
 
 impl Neuron {
-    fn composition(&self, inputs: &[f64]) -> f64 {
+    fn composition(&self, input: &[f64]) -> f64 {
         self.weights
             .iter()
-            .zip(inputs)
+            .zip(input)
             .map(|(w, x)| w*x)
             .sum::<f64>()
         + self.bias
@@ -46,9 +76,9 @@ impl Neuron {
         let mut rng = rand::thread_rng();
         let weights: Vec<f64>
             = (0..input_size)
-            .map(|_| rng.gen_range(-1.0..1.0))
+            .map(|_| rng.gen_range(-0.05..0.05))
             .collect();
-        let bias: f64 = rng.gen_range(-1.0..1.0);
+        let bias: f64 = rng.gen_range(-0.05..0.05);
 
         Self {
             weights,
@@ -56,22 +86,164 @@ impl Neuron {
         }
     }
 
-    pub fn forward(&self, inputs: &[f64]) -> f64 {
-        let z: f64 = self.composition(inputs);
+    pub fn forward(&self, input: &[f64]) -> f64 {
+        let z: f64 = self.composition(input);
 
         sigmoid(z)
     }
+}
 
-    pub fn train(&mut self, inputs: &[f64], target: f64, lr: f64) -> () {
-        let z: f64 = self.composition(inputs);
-        let output: f64 = sigmoid(z);
-        let error: f64 = output - target;
-        let delta = error * sigmoid_deriv(z);
 
-        for (idx, weight) in self.weights.iter_mut().enumerate() {
-            *weight -= lr * delta * inputs[idx];
+
+pub struct Layer {
+    pub neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    pub fn new(input_size: usize, layer_size: usize) -> Self {
+        let neurons: Vec<Neuron> = (0..layer_size)
+            .map(|_| Neuron::new(input_size))
+            .collect();
+
+        Self {
+            neurons
+        }
+    }
+
+    pub fn forward(&self, input: &[f64]) -> Vec<f64> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.forward(input))
+            .collect()
+    }
+
+    pub fn forward_with_softmax(&self, input: &[f64]) -> Vec<f64> {
+        let raw_output: Vec<f64> = self
+            .neurons
+            .iter()
+            .map(|neuron| neuron.composition(input))
+            .collect::<Vec<f64>>();
+
+        softmax(&raw_output)
+    }
+}
+
+
+
+pub struct Model {
+    pub layers: Vec<Layer>,
+}
+
+impl Model {
+    pub fn new(input_size: usize, output_size: usize) -> Self {
+        
+        let hidden1: Layer = Layer::new(input_size, 32);
+        let output: Layer = Layer::new(32, output_size);
+
+        Self {
+            layers: vec![
+                hidden1,
+                output
+            ],
+        }
+    }
+
+    pub fn forward(&self, input: &[f64]) -> Vec<f64> {
+        let mut out = input.to_vec();
+
+        for (idx, layer) in self.layers.iter().enumerate() {
+            if idx == self.layers.len()-1 {
+                out = layer.forward_with_softmax(&out);
+            } else {
+                out = layer.forward(&out);
+            }
         }
 
-        self.bias -= lr * delta;
+        out
+    }
+
+    pub fn train(&mut self, input: &[f64], target: &[f64], lr: f64) -> f64 {
+        let mut activations: Vec<Vec<f64>> = vec![input.to_vec()];
+        let mut zs: Vec<Vec<f64>> = vec![];
+        let mut out = input.to_vec();
+
+        for (idx, layer) in self.layers.iter().enumerate() {
+            let z: Vec<f64> = if idx == self.layers.len()-1 {
+                layer.neurons.iter()
+                    .map(|n| n.composition(&out))
+                    .collect()
+            } else {
+                layer.neurons.iter()
+                    .map(|n| n.composition(&out))
+                    .collect()
+            };
+        
+            zs.push(z.clone());
+        
+            out = if idx == self.layers.len()-1 {
+                softmax(&z)
+            } else {
+                z.iter().map(|&v| sigmoid(v)).collect()
+            };
+        
+            activations.push(out.clone());
+        }
+
+        let mut delta: Vec<f64> = activations
+            .last()
+            .unwrap()
+            .iter()
+            .zip(target.iter())
+            .map(|(o, t)| o-t)
+            .collect::<Vec<f64>>();
+
+        let num_layers = self.layers.len();
+
+        for idx in (0..num_layers).rev() {
+            let layer = &mut self.layers[idx];
+            let z = &zs[idx];
+            let a = &activations[idx];
+
+            let grad: Vec<f64> = if idx == num_layers - 1 {
+                delta.clone()
+            } else {
+                delta
+                    .iter()
+                    .zip(z.iter())
+                    .map(|(d, z)| d * sigmoid_deriv(*z))
+                    .collect::<Vec<f64>>()
+            };
+
+            for (jdx, neuron) in layer.neurons.iter_mut().enumerate() {
+                for kdx in 0..neuron.weights.len() {
+                    neuron.weights[kdx] -= lr * grad[jdx] * a[kdx];
+                }
+                neuron.bias -= lr * grad[jdx];
+            }
+
+            if idx > 0 {
+                let prev_layer_size = self.layers[idx-1].neurons.len();
+                let mut new_delta = vec![0.0; prev_layer_size];
+
+                for jdx in 0..new_delta.len() {
+                    for (kdx, neuron) in self.layers[idx].neurons.iter().enumerate() {
+                        new_delta[jdx] += grad[kdx] * neuron.weights[jdx];
+                    }
+                }
+                delta = new_delta;
+            }
+        }
+
+        activations
+            .last()
+            .unwrap()
+            .iter()
+            .zip(target.iter())
+            .map(|(o, t)| {
+                let o_safe = o.max(1e-10);
+                -(t * (o_safe).ln())
+            })
+            .sum::<f64>()
+        / target.len() as f64
     }
 }
